ddb1074da978cd9515dd58699a5cec4928abbaf2
Таня <nestana.1999@gmail.com>
2019-11-02 14:19:08 +0200

    Initial commit

--- src/com/company/util/Info.java
+++ src/com/company/util/Info.java
@@ -3,12 +3,10 @@ package com.company.util;
 public class Info {
 
     public void getInfo(String param) {
-
         System.out.print("Input the " + param + ": ");
     }
 
     public void getOutput(String board) {
-
         System.out.print(board);
     }
 }
